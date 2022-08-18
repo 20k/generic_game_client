621fa78fe97ec6d2b2c4c940fb9ea7a61f6be299
20k <icedshot@gmail.com>
2022-08-18 12:53:14 +0100

    cleanup

--- main.cpp
+++ main.cpp
@@ -11,95 +11,16 @@
 #include <imgui/misc/freetype/imgui_freetype.h>
 #include <imgui/misc/cpp/imgui_stdlib.h>
 
-template<typename T>
-struct async_queue
+int main()
 {
-    std::mutex mut;
-    std::vector<T> data;
+    connection_settings csett;
 
-    void push(const T& d)
-    {
-        std::lock_guard guard(mut);
-        data.push_back(d);
-    }
-
-    bool has_front()
-    {
-        std::lock_guard guard(mut);
-        return data.size() > 0;
-    }
-
-    T pop_front()
-    {
-        std::lock_guard guard(mut);
-        T val = data.front();
-        data.erase(data.begin());
-        return val;
-    }
-};
-
-void connection_thread(async_queue<nlohmann::json>& server_messages, async_queue<std::string>& client_messages)
-{
-    connection_settings sett;
-
-    connection_send_data send(sett);
+    connection_send_data send(csett);
     connection_received_data recv;
 
     connection conn;
     conn.connect("127.0.0.1", 6600, connection_type::SSL);
 
-    bool connected = false;
-
-    while(1)
-    {
-        if(!connected && conn.connection_pending())
-        {
-            //std::cout << "Connected\n";
-
-            nlohmann::json js;
-            js["type"] = 0;
-            js["msg"] = "connected";
-
-            server_messages.push(js);
-
-            connected = true;
-        }
-
-        conn.receive_bulk(recv);
-
-        for(auto& [id, datas] : recv.websocket_read_queue)
-        {
-            for(const write_data& dat : datas)
-            {
-                nlohmann::json js = nlohmann::json::parse(dat.data);
-
-                server_messages.push(js);
-            }
-        }
-
-        while(client_messages.has_front())
-        {
-            std::string dat = client_messages.pop_front();
-
-            nlohmann::json js;
-            js["type"] = 0;
-            js["msg"] = dat;
-
-            write_data wdata;
-            wdata.id = -1;
-            wdata.data = js.dump();
-
-            send.write_to_websocket(wdata);
-        }
-
-        conn.send_bulk(send);
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-}
-
-int main()
-{
     render_settings sett;
     sett.width = 800;
     sett.height = 600;
@@ -119,26 +40,68 @@ int main()
     io.Fonts->Clear();
     io.Fonts->AddFontFromFileTTF("DosFont.ttf", 16, &font_cfg);
 
-    async_queue<std::string> client_messages;
-    async_queue<nlohmann::json> server_messages;
-
-    std::thread(connection_thread, std::ref(server_messages), std::ref(client_messages)).detach();
+    std::vector<std::string> client_messages;
+    std::vector<nlohmann::json> server_messages;
 
     std::vector<std::string> text_history;
+
+    bool connected = false;
 
     std::string command;
 
     while(1)
     {
+        if(!connected && conn.connection_pending())
+        {
+            nlohmann::json js;
+            js["type"] = 0;
+            js["msg"] = "connected";
+
+            server_messages.push_back(js);
+
+            connected = true;
+        }
+
+        conn.receive_bulk(recv);
+
+        for(auto& [id, datas] : recv.websocket_read_queue)
+        {
+            for(const write_data& dat : datas)
+            {
+                nlohmann::json js = nlohmann::json::parse(dat.data);
+
+                server_messages.push_back(js);
+            }
+        }
+
+        while(client_messages.size() > 0)
+        {
+            std::string dat = client_messages.front();
+            client_messages.erase(client_messages.begin());
+
+            nlohmann::json js;
+            js["type"] = 0;
+            js["msg"] = dat;
+
+            write_data wdata;
+            wdata.id = -1;
+            wdata.data = js.dump();
+
+            send.write_to_websocket(wdata);
+        }
+
+        conn.send_bulk(send);
+
         window.poll();
 
         ImGui::Begin("Hi there", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
 
         //ImGui::Text("Test Text");
 
-        while(server_messages.has_front())
+        while(server_messages.size() > 0)
         {
-            nlohmann::json js = server_messages.pop_front();
+            nlohmann::json js = server_messages.front();
+            server_messages.erase(server_messages.begin());
 
             std::string msg = js["msg"];
 
@@ -152,7 +115,7 @@ int main()
 
         if(ImGui::InputText("Input", &command, ImGuiInputTextFlags_EnterReturnsTrue))
         {
-            client_messages.push(command);
+            client_messages.push_back(command);
             command.clear();
         }
 
